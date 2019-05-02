fb1487f3f614f8d837b2cbc51ff83381817c229d
David Gardiner <david@gardiner.net.au>
2019-05-02 21:46:17 +0930

    NullableReferenceTypes

---
+++ NullableReferenceTypes/BusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class BusinessLogic
+    {
+        public void Process(string text)
+        {
+            if (int.TryParse(text, out var result))
+            {
+                Console.WriteLine(result * 100);
+            }
+            else
+            {
+                if (text.Length > 5)
+                {
+                    Console.WriteLine(text);
+                }
+                else
+                {
+                    var s = "'" + text + "'";
+
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ NullableReferenceTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bl = new BusinessLogic();
+
+            bl.Process("1234");
+            bl.Process("Adelaide");
+            bl.Process(null);
+
+            string s = null;
+
+            var c = s[1];
+        }
+    }
+}
