5ff543f94d58452598e5a0af5255b7381cb8dfb8
David Gardiner <david@gardiner.net.au>
2019-05-03 21:18:10 +0930

    Null coalescing assignment

---
+++ NullCoalescingAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NullCoalescingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://github.com/dotnet/csharplang/blob/master/proposals/csharp-8.0/null-coalescing-assignment.md
+
+            string y = null;
+
+            if (y == null)
+            {
+                y = "def";
+            }
+
+            #region Assignment
+            // y ??= "def";
+            #endregion
+
+            Console.WriteLine(y);
+        }
+    }
+}
