54f04467312edd8dadf65411c2613f2d8ab19392
David Gardiner <david@gardiner.net.au>
2019-05-03 20:55:14 +0930

    verbatin interpolated strings

---
+++ InterpolatedStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterpolatedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verbatim interpolated strings
+            // https://github.com/dotnet/csharplang/issues/1630
+
+            Console.WriteLine(@$"Hello World {DateTime.Now}");
+            Console.WriteLine($@"Hello World {DateTime.Now}");
+        }
+    }
+}
