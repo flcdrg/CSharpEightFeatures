4ad6f91fdef5b5b51581409aab7c04a61ccd9ac3
David Gardiner <david@gardiner.net.au>
2019-05-03 20:29:24 +0930

    Indices and ranges

---
+++ IndicesAndRanges/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IndicesAndRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Adelaide .NET User Group";
+
+            var location = name.Substring(0, 8);
+            var kind = name.Substring(name.Length - 5, 5);
+
+            // var index = ^5;
+            // var range = 3..4;
+
+            Console.WriteLine(location);
+            Console.WriteLine(kind);
+        }
+    }
+}
