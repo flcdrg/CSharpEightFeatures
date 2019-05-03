b08ccad63687a7025a8b252b610d4d2ed9a58d08
David Gardiner <david@gardiner.net.au>
2019-05-03 22:18:18 +0930

    Enhanced usings

---
+++ EnhancedUsing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace EnhancedUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://github.com/dotnet/csharplang/blob/master/meetings/2018/LDM-2018-07-09.md
+            using (var stream = File.Create(Path.GetTempFileName()))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    streamWriter.WriteLine("Hello world");
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+}
